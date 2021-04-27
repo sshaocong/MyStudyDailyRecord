25f2b2572e5a888066d2c89b50851da0d9929d41
sshaocong <644138389@qq.com>
2021-04-27 15:16:43 +0800

    将地雷标记改为'#' 并且在游戏失败后打印地雷地图供玩家看

--- clearMines.c
+++ clearMines.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define  _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -9,18 +9,19 @@
 #define MAX_COL 9
 #define MINE_COUNT 10
 
-void init(char showMap[MAX_ROW][MAX_COL], char mineMap[MAX_ROW][MAX_COL]) {
+void init(char showMap[MAX_ROW][MAX_COL], char mineMap[MAX_ROW]
+	[MAX_COL]) {
 	memset(showMap, '*', MAX_ROW * MAX_COL);
 	memset(mineMap, '0', MAX_ROW * MAX_COL);
-	srand((unsigned int)time(0));
 	int mineCount = 0;
-	while (mineCount<=MINE_COUNT) {
+	srand((unsigned int)time(0));
+	while (mineCount <= MINE_COUNT) {
 		int row = rand() % MAX_ROW;
 		int col = rand() % MAX_COL;
-		if (mineMap[row][col] == '1') {
+		if (mineMap[row][col] == '#') {
 			continue;
 		}
-		mineMap[row][col] = '1';
+		mineMap[row][col] = '#';
 		mineCount++;
 	}
 }
@@ -28,7 +29,7 @@ void init(char showMap[MAX_ROW][MAX_COL], char mineMap[MAX_ROW][MAX_COL]) {
 void print(char theMap[MAX_ROW][MAX_COL]) {
 	for (int row = 0; row < MAX_ROW; row++) {
 		for (int col = 0; col < MAX_COL; col++) {
-			printf("%c ",theMap[row][col]);
+			printf("%c ", theMap[row][col]);
 		}
 		printf("\n");
 	}
@@ -37,22 +38,21 @@ void print(char theMap[MAX_ROW][MAX_COL]) {
 void update(char showMap[MAX_ROW][MAX_COL], char mineMap[MAX_ROW][MAX_COL],
 	int row, int col) {
 	int count = 0;
-	for (int r = row - 1; r <= row + 1; r++) {
-		for (int c = col - 1; c <= col + 1; c++) {
+	for (int r = row - 1; r < row + 1; r++) {
+		for (int c = col - 1; c < col + 1; c++) {
 			if (r < 0 || r >= MAX_ROW || c < 0 || c >= MAX_COL) {
 				continue;
 			}
 			if (r == row && c == col) {
 				continue;
 			}
-			if (mineMap[r][c] == '1') {
+			if (mineMap[r][c] == '#') {
 				count++;
 			}
 		}
 	}
 	showMap[row][col] = '0' + count;
 }
-
 int main() {
 	char showMap[MAX_ROW][MAX_COL];
 	char mineMap[MAX_ROW][MAX_COL];
@@ -63,26 +63,28 @@ int main() {
 		print(showMap);
 		int row = 0;
 		int col = 0;
-		printf("请输入坐标(row col):");
+		printf("please input(row col):");
 		scanf("%d %d", &row, &col);
 		if (row < 0 || row >= MAX_ROW || col < 0 || col >= MAX_COL) {
-			printf("输入非法!!请重新输入!!\n");
+			printf("INPUT WRONG!!!please input again!!\n");
 			Sleep(1000);
 			continue;
 		}
 		if (showMap[row][col] != '*') {
-			printf("输入坐标已翻开!!请重新输入!!\n");
+			printf("HAVE INPUT!!!!please input again!!\n");
 			Sleep(1000);
 			continue;
 		}
-		if (mineMap[row][col] == '1') {
-			printf("踩雷了!!游戏失败!!");
+		if (mineMap[row][col] == '#') {
+			printf("GAME OVER!!!\n");
+			print(mineMap);
 			break;
 		}
 		update(showMap, mineMap, row, col);
 		openedCount++;
 		if (openedCount == MAX_ROW * MAX_COL - MINE_COUNT) {
-			printf("游戏胜利!!");
+			printf("WINNER WINNER CHICKEN CHICKEN!!~");
+			break;
 		}
 	}
 	return 0;
