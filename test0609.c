71ef2ce4982dee4aa0edf7a46077ed480fc35b8f
sshaocong <644138389@qq.com>
2021-06-09 18:30:04 +0800

    C基础刷题

---
+++ test0609.c
@@ -0,0 +1,172 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+
+
+	/*int n = 0;
+	int h = 0;
+	int m = 0;
+
+	while ((scanf("%d %d %d", &n, &h, &m) != EOF) ){
+		int drink = m / h;
+		if (m % h) {
+			drink++;
+		}
+		int sum = n - drink;
+		printf("%d", sum);
+	}*/
+
+	//double a = 0.0;
+	//double b = 0.0;
+	//double c = 0.0;
+	//scanf("%lf %lf %lf", &a, &b, &c);
+	//double sum = (a + b + c);
+	//double aver = (a + b + c) / 3;
+	//printf("%.2lf %.2lf", sum, aver);
+	////int seconds = 0;
+	//scanf("%d", &seconds);
+	////小时
+	//if (seconds / 3600) {
+	//	printf("%d ", seconds / 3600);
+	//}
+	//else {
+	//	printf("0 ");
+	//}
+	////分钟
+	//if ((seconds % 3600) / 60) {
+	//	printf("%d ", (seconds % 3600) / 60);
+	//}
+	//else {
+	//	printf("0 ");
+	//}
+	////秒
+	//if ((seconds % 3600) % 60) {
+	//	printf("%d", (seconds % 3600) % 60);
+	//}
+	//else {
+	//	printf("0");
+	//}
+	//int a = 0;
+	//int b = 0;
+	//scanf("%d %d", &a, &b);
+	//printf("%d", (a + b)%100);
+	//for (int i = 10000; i < 100000; i++) {
+	//	int sum = 0;
+	//	for (int j = 10; j <= 10000; j *= 10) {
+	//		int a = i / j;
+	//		int b = i % j;
+	//		int c = a * b;
+	//		sum = sum + c;
+	//	}
+	//	if (sum == i) {
+	//		printf("%d ", i);
+	//	}
+	//}
+
+	//char ch = 0;
+	//while ((ch = getchar()) != EOF) {
+	//	printf("%c\n", (ch + 32));
+	//	getchar();
+	//}
+	//double a = 0.0;
+	//scanf("%lf", &a);
+	//double pi = 3.1415926;
+	//double b = (4.0 / 3.0);
+	//double V = b * pi * pow(a,3);
+	//printf("%.3f", V);
+
+	//int a = 0;
+	//int b = 0;
+	//int c = 0;
+	//scanf("%d %d %d", &a, &b, &c);
+	//double cir = (a + b + c);
+	//double p = (double)(a + b + c) / 2;
+	//double s = (double)sqrt(p * (p - a) * (p - b) * (p - c));
+	//printf("circumference=%.2f area=%.2f", cir, s);
+	//int a = 0;
+	//int b = 0;
+	//scanf("%d %d", &a, &b);
+	//double BMI = a / ((double)b/100 * b/100);
+	//printf("%.2f", BMI);
+	//int a = 0;
+	//int b = 0;
+	//scanf("%d %d", &a, &b);
+	//printf("%d %d", a / b, a % b);
+	//int a = 40;
+	//int c = 212;
+	//int d = (-8 + 22) * a - 10 + c / 2;
+	//printf("%d", d);
+	//int a = 0;
+	//while ((scanf("%d", &a)) != EOF) {
+	//	printf("%d", 1 << a);
+	//}
+	//int a = 0;
+	//int b = 0;
+	//int ret = scanf("a=%d,b=%d", &a, &b);
+	//int c = 0;
+	//c = a;
+	//a = b;
+	//b = c;
+	//printf("a=%d,b=%d", a, b);
+	//int year = 0;
+	//int month = 0;
+	//int date = 0;
+	//scanf("%4d%2d%2d", &year, &month, &date);
+	//printf("year=%4d\nmonth=%02d\ndate=%02d\n", year, month, date);
+	//int arr[] = { 73,32,99,97,110,32,100,111,32,105,116,33 };
+	//int size = sizeof(arr) / sizeof(arr[0]);
+	//for (int i = 0; i < size; i++) {
+	//	printf("%c", arr[i]);
+	//}
+	//char ch = 0;
+	//scanf("%c",&ch);
+	//for (int i = 0; i < 5; i++) {
+	//	for (int j = 0; j < 5 - i-1; j++) {
+	//		printf(" ");
+	//	}
+	//	for (int j = 0; j <= i; j++) {
+	//		printf("%c ", ch);
+	//	}
+	//	printf("\n");
+	//}
+
+
+	//int ch = 0;
+	//while ((ch = getchar()) != EOF) {
+	//	getchar();
+	//	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+	//		printf("YES");
+	//	}
+	//	else {
+	//		printf("NO");
+	//	}
+	//	/*getchar();*/
+	//}
+	//char password[256];
+	//printf("请输入密码:");
+	//scanf("%s", password);
+	//getchar();
+	//printf("请验证密码:");
+	//printf("\n");
+	//int ch = getchar();
+	//if (ch == 'Y') {
+	//	printf("验证成功");
+	//}
+	//else {
+	//	printf("验证失败");
+	//}
+
+	//int ret = printf("hello world\n");
+	//printf("%d\n", ret);
+	//
+
+	//int ch = 0;
+	//while ((ch = getchar()) != EOF) {
+	//	printf("%c\n", (ch + 32));
+	//	getchar();
+	//}
+
+	return 0;
+}
